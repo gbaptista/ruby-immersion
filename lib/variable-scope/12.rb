dc8191508fbda674091caf1488fca2ab4cefaa12
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2013-02-13 22:50:40 -0200

    VariableScope_12

---
+++ lib/variable-scope/12.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+class VariableScope_12
+
+  @@class_var = 'class_var'
+
+  def self.class_var; @@class_var; end
+
+  def self.class_var=(value); @@class_var = value; end
+
+  def class_var; @@class_var; end
+
+  def class_var=(value); @@class_var = value; end
+
+end
---
+++ test/variable-scope/test_12.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+require 'test/unit'
+require 'include_file'
+
+IncludeFile::inject __FILE__
+
+class LoveTest_VariableScope_12 < Test::Unit::TestCase
+
+  def test_with_love
+
+    # São tratadas de forma isolada entre métodos de classe e as
+    # diferentes instâncias da classe:
+
+    instance_01 = VariableScope_12.new
+    instance_02 = VariableScope_12.new
+
+    assert_equal('class_var', VariableScope_12.class_var)
+    assert_equal('class_var', instance_01.class_var)
+    assert_equal('class_var', instance_02.class_var)
+
+    VariableScope_12.class_var = 'Has he lost his mind?'
+    instance_01.class_var = 'Can he see or is he blind?'
+    instance_02.class_var = 'He was turned to steel'
+
+    assert_equal('He was turned to steel', VariableScope_12.class_var)
+    assert_equal('He was turned to steel', instance_01.class_var)
+    assert_equal('He was turned to steel', instance_02.class_var)
+    assert_equal('He was turned to steel', VariableScope_12.new.class_var)
+
+  end
+
+end
